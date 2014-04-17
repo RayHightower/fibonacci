3ad001400499cf308a5a322c76fbb2d3a53f60b3
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-17 16:12:03 -0500

    experiment with tail recursion and randomization

--- fibo-tr.rb
+++ fibo-tr.rb
@@ -1,4 +1,5 @@
 # Calculate the nth Fibonacci number, f(n). Using invariants
+# Originally written by Manuel Vidaurre, https://gist.github.com/mvidaurre/11006570
 def fibo_tr(n, acc1, acc2)
   if n == 0
     0
@@ -14,6 +15,14 @@ def fibo (n)
 end 
  
 # Display the Fibonacci sequence.
-(1..40).each do |number|
+(1..50).each do |number|
+  puts "fibo(#{number}) = #{fibo(number)}"
+end
+
+# Display 50 Fibonacci numbers in a random sequence.
+number_of_values = 50
+puts "\nNow, display #{number_of_values} Fibonacci numbers in random sequence..."
+number_of_values.times do
+  number = (rand*number_of_values + 1).to_i
   puts "fibo(#{number}) = #{fibo(number)}"
 end
