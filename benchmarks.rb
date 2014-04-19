aede005b8f371edf7b2106ee31320096468351ee
Jon Allured <jon.allured@gmail.com>
2014-04-19 15:59:00 -0500

    Add benchmark code

---
+++ benchmarks.rb
@@ -0,0 +1,27 @@
+require 'benchmark'
+
+require './fibo-raw'
+require './fibo-memo'
+require './fibo-tr'
+
+max = 10
+
+Benchmark.bmbm do |b|
+  b.report('RawFibo') do
+    (1..max).each do |n|
+      RawFibo.calculate n
+    end
+  end
+
+  b.report('MemoFibo') do
+    (1..max).each do |n|
+      MemoFibo.calculate n
+    end
+  end
+
+  b.report('TailFibo') do
+    (1..max).each do |n|
+      TailFibo.calculate n
+    end
+  end
+end
