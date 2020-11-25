e0a0ab19962db09b67bab0beef7de196daa1b021
Raymond T Hightower <rth@rayhightower.com>
2020-11-24 21:47:11 -0700

    Automation rocks.

--- fibo-memo.rb
+++ fibo-memo.rb
@@ -12,26 +12,12 @@ class MemoFibo
     end
   end
 
-<<<<<<< HEAD
   def self.max_fibo_size
     100
   end
 
   def self.scratchpad
     @scratchpad ||= build_scratchpad
-=======
-# Calculate the nth Fibonacci number, f(n).
-def fibo (n)
-  @fibo_cache ||= {}
-  @fibo_cache.fetch(n) do
-    if n <= 1
-      value = n
-    else
-      value = fibo(n-1) + fibo(n-2)
-    end
-
-    @fibo_cache[n] = value
->>>>>>> 3560858467d3e2bb7f5f9efceafa8dcd0ecdc52e
   end
 
   def self.build_scratchpad
